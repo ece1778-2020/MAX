44d18e37ca3966c09d63dd3ba126250b80d072fa
felipe-clark <felipe.clark@gmail.com>
2020-02-23 21:46:11 -0500

    Add signup flow

---
+++ app/src/main/java/com/clarksoft/max/InputValidation.java
@@ -0,0 +1,35 @@
+package com.clarksoft.max;
+
+import com.google.android.material.textfield.TextInputLayout;
+import java.util.regex.Pattern;
+
+class InputValidation {
+
+    static final Pattern PWD_PATTERN =  Pattern.compile("^" +
+            "(?=.*[0-9])" +         //at least 1 digit
+            "(?=.*[a-z])" +         //at least 1 lower case letter
+            "(?=.*[A-Z])" +         //at least 1 upper case letter
+            "(?=.*[!@#$%^&*()_+={}<>.,?/|';:`~\\[\\]-])" +    //at least 1 special character
+            "(?=\\S+$)" +           //no white spaces
+            ".{8,}" +               //at least 8 characters
+            "$");
+    static final Pattern USERNAME_PATTERN = Pattern.compile("([a-zA-Z].*?){3}");
+
+
+    Boolean matchPattern(String input, Pattern pattern){
+        return pattern.matcher(input).matches();
+    }
+
+    void updateFieldError(Boolean ok, TextInputLayout view, String msg) {
+        if(ok) {
+            view.setError(null);
+        }
+        else {
+            view.setError(msg);
+        }
+    }
+
+    Boolean comparePwd(String pwd1, String pwd2) {
+        return pwd1.equals(pwd2);
+    }
+}
--- app/src/main/java/com/clarksoft/max/LoginActivity.java
+++ app/src/main/java/com/clarksoft/max/LoginActivity.java
@@ -1,29 +1,96 @@
 package com.clarksoft.max;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity implements OnCompleteListener<AuthResult> {
+
+    private static final String LOG_TAG = LoginActivity.class.getName();
+
+    private TextInputLayout email;
+    private TextInputLayout password;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.login_email);
+        password = findViewById(R.id.login_password);
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
     public void onButtonClick(View view) {
+        Intent intent;
+
         switch (view.getId()){
             case R.id.login_btn_login:
-                Intent intent = new Intent(this, NavigationActivity.class);
+
+                EditText emailEdt = email.getEditText();
+                EditText passwordEdt = password.getEditText();
+
+                // If null happens, something critical happened and
+                // it is best to wait for the user to try again
+                if (emailEdt != null && passwordEdt != null) {
+                    String emailInput = emailEdt.getText().toString();
+                    String passwordInput = passwordEdt.getText().toString();
+
+                    if (emailInput.isEmpty() || passwordInput.isEmpty()) {
+                        updateUI(null);
+                        return;
+                    }
+
+                    mAuth.signInWithEmailAndPassword(emailInput, passwordInput).addOnCompleteListener(this);
+                }
+
+                intent = new Intent(this, NavigationActivity.class);
                 startActivity(intent);
                 break;
             case R.id.login_btn_sign_up:
+                intent = new Intent(this, SignupActivity.class);
+                startActivity(intent);
                 break;
+        }
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+            Intent intent = new Intent(this, NavigationActivity.class);
+            startActivity(intent);
+        } else {
+            Toast toast = Toast.makeText(getApplicationContext(), getString(R.string.login_err_login_failed), Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+    @Override
+    public void onComplete(@NonNull Task task) {
+        if (task.isSuccessful()) {
+            // Sign in success, update UI with the signed-in user's information
+            Log.d(LOG_TAG, "signInWithEmail:success");
+            FirebaseUser user = mAuth.getCurrentUser();
+            updateUI(user);
+        } else {
+            // If sign in fails, display a message to the user.
+            Log.w(LOG_TAG, "signInWithEmail:failure", task.getException());
+            updateUI(null);
         }
     }
 }
