dc855a67c23585c11247976ae6eb1b4ee8588567
haldarritam <haldarritam@gmail.com>
2020-03-24 22:09:52 -0400

    Added the progression notification and did some minor touch-ips.

--- app/src/main/java/com/clarksoft/max/SessionFragment.java
+++ app/src/main/java/com/clarksoft/max/SessionFragment.java
@@ -2,6 +2,7 @@ package com.clarksoft.max;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Color;
@@ -11,6 +12,7 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.cardview.widget.CardView;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
@@ -23,8 +25,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RadioButton;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -43,6 +47,7 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Locale;
@@ -115,6 +120,11 @@ public class SessionFragment extends Fragment implements OnCompleteListener<Quer
 
     enum mediaPlayers {slow, fast};
     private Boolean sound_enabled = false;
+
+    private final Integer num_days = 30;
+    private Float total_in_hr = 0.0f;
+    private Integer num_sessions = 0, target_time = 0, oldest_date = 0;
+    ImageView session_notification_active;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -268,6 +278,9 @@ public class SessionFragment extends Fragment implements OnCompleteListener<Quer
         session_btn_pause.setVisibility(View.INVISIBLE);
         session_btn_end.setVisibility(View.INVISIBLE);
 
+        session_notification_active = view.findViewById(R.id.session_notification_active);
+        session_notification_active.setVisibility(View.INVISIBLE);
+
         session_btn_start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -319,6 +332,10 @@ public class SessionFragment extends Fragment implements OnCompleteListener<Quer
                     cmr_offset += 1;
             }
         });
+
+
+        checkTimeIncreaseSuggestion();
+
 
         return view;
     }
@@ -509,6 +526,7 @@ public class SessionFragment extends Fragment implements OnCompleteListener<Quer
                     if (document != null && document.exists()) {
                         max_hr = Integer.parseInt(document.get("max_hr").toString());
                         min_hr = Integer.parseInt(document.get("min_hr").toString());
+                        target_time = Integer.parseInt(document.get("target_time").toString());
                     } else {
                         Log.e("DB", "Document does not exist.");
                     }
@@ -675,5 +693,127 @@ public class SessionFragment extends Fragment implements OnCompleteListener<Quer
         } finally {
             audio_mutex.unlock();
         }
+    }
+
+    private void checkTimeIncreaseSuggestion(){
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_YEAR, num_days * (-1));
+        String target_date = String.valueOf(calendar.get(Calendar.YEAR)) +
+                String.format("%02d", calendar.get(Calendar.MONTH) + 1) +
+                String.valueOf(calendar.get(Calendar.DATE));
+
+        int current_date = Integer.parseInt(new SimpleDateFormat("yyyyMMdd", Locale.CANADA).format((new Date())));
+        int one_month_back = Integer.parseInt(target_date);
+
+        Query userDataQuery = db.collection("session")
+                .whereEqualTo("uid", userUUID)
+                .orderBy("date")
+                .limit(1);
+        userDataQuery.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    QuerySnapshot session_data = task.getResult();
+
+                    for (QueryDocumentSnapshot document : session_data) {
+                        oldest_date = Integer.parseInt(document.get("date").toString());
+                    }
+
+                    if(oldest_date <= one_month_back) {
+
+                        Query userDataQuery = db.collection("session")
+                                .whereEqualTo("uid", userUUID)
+                                .orderBy("date")
+                                .startAt(one_month_back)
+                                .endAt(current_date);
+                        userDataQuery.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    QuerySnapshot session_data = task.getResult();
+
+                                    for (QueryDocumentSnapshot document : session_data) {
+                                        num_sessions++;
+                                        total_in_hr += Integer.parseInt(document.get("in_hr").toString()) / 60.0f;
+                                    }
+
+                                    num_sessions = (num_sessions>4)?num_sessions:4;
+
+                                    Float avg_session_time = total_in_hr / num_sessions;
+
+                                    DocumentReference userData = db.collection("settings1").document(userUUID);
+                                    userData.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                            if (task.isSuccessful()) {
+                                                DocumentSnapshot document = task.getResult();
+                                                if (document != null && document.exists()) {
+                                                    target_time = Integer.parseInt(document.get("target_time").toString());
+                                                } else {
+                                                    Log.e("DB", "Document does not exist.");
+                                                }
+                                            } else {
+                                                Log.e("DB", "", task.getException());
+                                            }
+
+
+                                            if (avg_session_time >= target_time && avg_session_time < 15) {
+                                                checkExerciseDays("Think about setting your \"Exercise goal\" in the User Settings page to 15 minutes in target HR range per week");
+                                            } else if (avg_session_time >= target_time && avg_session_time >= 15 && avg_session_time < 30) {
+                                                checkExerciseDays("Think about setting your \"Exercise goal\" in the User Settings page to 30 minutes in target HR range per week");
+                                            } else if (avg_session_time >= target_time && avg_session_time >= 30 && avg_session_time < 45) {
+                                                checkExerciseDays("Think about setting your \"Exercise goal\" in the User Settings page to 45 minutes in target HR range per week");
+                                            } else if (avg_session_time >= target_time && avg_session_time >= 45 && avg_session_time < 60) {
+                                                checkExerciseDays("Think about setting your \"Exercise goal\" in the User Settings page to 60 minutes in target HR range per week");
+                                            } else if (avg_session_time >= target_time && avg_session_time >= 60 && avg_session_time < 75) {
+                                                checkExerciseDays("Think about setting your \"Exercise goal\" in the User Settings page to 75 minutes in target HR range per week");
+                                            } else if (avg_session_time >= target_time && avg_session_time >= 75 && avg_session_time < 90) {
+                                                checkExerciseDays("Think about setting your \"Exercise goal\" in the User Settings page to 90 minutes in target HR range per week");
+                                            }
+                                            else{
+                                                checkExerciseDays("");
+                                            }
+
+                                        }
+                                    });
+
+                                } else {
+                                    Log.e("DB", "", task.getException());
+                                }
+                            }
+                        });
+                    }
+                } else {
+                    Log.e("DB", "", task.getException());
+                }
+            }
+        });
+    }
+
+    private void checkExerciseDays(String target_suggestion){
+
+
+        if(!target_suggestion.isEmpty()) {
+            session_notification_active.setVisibility(View.VISIBLE);
+            session_notification_active.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getContext());
+                    alertDialogBuilder.setMessage(target_suggestion);
+                    alertDialogBuilder.setNegativeButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            session_notification_active.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                    AlertDialog alertDialog = alertDialogBuilder.create();
+                    alertDialog.show();
+
+                }
+            });
+        }
+
     }
 }
